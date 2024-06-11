ce5d88862895e3d26df6503cc3939ba7234183c0
Gordon Shumway <pinaev5@mail.ru>
2024-06-12 01:24:58 +0300

    new version of logic - a lot of changes

--- planning_tools.py
+++ planning_tools.py
@@ -1,6 +1,6 @@
 import datetime
 import heapq
-from typing import List, Dict
+from typing import List, Dict, Tuple, Optional
 import itertools
 
 class Ship:
@@ -78,12 +78,56 @@ def calculate_caravan_travel_time(caravan: Caravan) -> int:
     # returns travel time in days
     pass
 
+
+'''
+----------------------------------------------------------------------------------------------
+Class for storing planned routes, dates and included ships
+----------------------------------------------------------------------------------------------
+add_route          - add new planned route with all required info about it
+get_routes_by_date - return all routes planned for current date (departing/arriving)
+'''
+class RouteSchedule:
+    def __init__(self):
+        self.routes = []
+
+    def add_route(self, departure_date: datetime.date, arrival_date: datetime.date, 
+                  departure_port: str, arrival_port: str, movement_type: str, participants: List[str]):
+        route = {
+            'departure_date': departure_date,
+            'arrival_date': arrival_date,
+            'departure_port': departure_port,
+            'arrival_port': arrival_port,
+            'movement_type': movement_type,
+            'participants': participants
+        }
+        self.routes.append(route)
+
+    def get_routes_by_date(self, date: datetime.date) -> List[Dict]:
+        return [route for route in self.routes if route['departure_date'] == date or route['arrival_date'] == date]
+
+'''
+----------------------------------------------------------------------------------------------
+Class for whole planning process
+----------------------------------------------------------------------------------------------
+__init__                    - initialize parameters for system
+run_daily_planning          - daily iteration for schedule, planning and ships status update
+check_new_applications      - adding application online
+check_ice_conditions        - check ice condition changes
+optimize_routes             - routes optimization for ships that are already on route (can be triggered by ice chacnges)
+plan_shipments              - departure planning with/without icebreaker for each port
+plan_caravan_shipment       - if there are icebreakers in port, plan caravan shipment
+group_ships_by_destination  - group ships from one port 
+find_best_caravan           - find most time-profitable combination of ships to form a caravan with cur icebreaker
+
+
+'''
 class PlanningSystem:
     def __init__(self, ports: List[Port], max_in_caravan: int, max_icebreakers: int, current_date: datetime.date):
         self.ports = {port.port_name: port for port in ports}
         self.max_in_caravan = max_in_caravan
         self.max_icebreakers = max_icebreakers
         self.current_date = current_date
+        self.schedule = RouteSchedule()
 
     def run_daily_planning(self):
         while True:
@@ -112,29 +156,23 @@ class PlanningSystem:
 
     def plan_shipments(self):
         for port_name, port in self.ports.items():
-            self.plan_port_shipments(port)
+            if port.icebreakers:
+                self.plan_caravan_shipment(port)
+        
+        for port_name, port in self.ports.items():
             self.check_icebreaker_availability(port)
+            self.plan_remaining_ships(port)
 
-    def plan_port_shipments(self, port: Port):
+    def plan_caravan_shipment(self, port: Port):
         # Step 1: Group ships by destination
         destination_groups = self.group_ships_by_destination(port.ships)
 
         # Step 2: Form caravans for each destination group
         for destination, ships in destination_groups.items():
-            best_caravan = None
-            best_quality = float('-inf')
+            best_caravan = self.find_best_caravan(ships, port.icebreakers)
 
-            # Generate all possible combinations of ships for sizes from 1 to max_in_caravan
-            for size in range(1, self.max_in_caravan + 1):
-                for comb in itertools.combinations(ships, size):
-                    for icebreaker in port.icebreakers:
-                        caravan = Caravan(list(comb), icebreaker, self.max_in_caravan)
-                        if caravan.caravan_quality > best_quality:
-                            best_caravan = caravan
-                            best_quality = caravan.caravan_quality
-
-            if best_caravan and len(port.icebreakers) > 0:
-                self.depart_caravan(best_caravan, port)
+            if best_caravan:
+                self.record_caravan_route(best_caravan, port)
 
     def group_ships_by_destination(self, ships: List[Ship]) -> Dict[str, List[Ship]]:
         groups = {}
@@ -144,58 +182,148 @@ class PlanningSystem:
             groups[ship.destination].append(ship)
         return groups
 
-    def depart_caravan(self, caravan: Caravan, port: Port):
-        # Update statuses of ships and icebreaker in the caravan
-        for ship in caravan.ships:
-            port.ships.remove(ship)
-            ship.caravan = caravan
+    def find_best_caravan(self, ships: List[Ship], icebreakers: List[Icebreaker]) -> Tuple[Optional[Caravan], float]:
+        best_caravan = None
+        best_quality = float('-inf')
 
-        port.icebreakers.remove(caravan.icebreaker)
-        caravan.icebreaker.current_port = None
+        # Generate all possible combinations of ships for sizes from 1 to max_in_caravan
+        for size in range(1, self.max_in_caravan + 1):
+            for comb in itertools.combinations(ships, size):
+                if None in comb:
+                    continue
+                for icebreaker in icebreakers:
+                    caravan = Caravan(list(comb), icebreaker, self.max_in_caravan)
+                    if caravan.caravan_quality > best_quality:
+                        best_caravan = caravan
+                        best_quality = caravan.caravan_quality
 
-        # Set departure and arrival dates
-        caravan.departure_date = max(ship.ready_date for ship in caravan.ships)
-        arrival_date = caravan.departure_date + datetime.timedelta(days=calculate_caravan_travel_time(caravan))
-        caravan.icebreaker.available_date = arrival_date
-        self.ports[caravan.ships[0].destination].add_arriving_icebreaker(caravan.icebreaker, arrival_date)
-
-        print(f"[LOG] Caravan with icebreaker {caravan.icebreaker.icebreaker_id} departs from {port.port_name} on {caravan.departure_date} to {caravan.ships[0].destination} arriving at {arrival_date}.")
-
+        return best_caravan #, best_quality
 
     def check_icebreaker_availability(self, port: Port):
-        if port.icebreakers:
-            self.reassign_icebreaker(port)
+        for icebreaker in port.icebreakers:
+            if icebreaker.available_date <= self.current_date:
+                best_port, best_value = None, float('-inf')
+
+                if port.ships:
+                    best_caravan = self.find_best_caravan(port.ships, port.icebreakers)
+                    best_value = best_caravan.caravan_quality
+
+                for other_port in self.ports.values():
+                    if other_port == port:
+                        continue
+                    best_caravan = self.find_best_caravan(other_port.ships, other_port)
+                    if best_caravan:
+                        travel_time = self.calculate_icebreaker_travel_time_to_port(icebreaker, other_port)
+                        port_value = best_caravan.caravan_quality - travel_time
+                        if port_value > best_value:
+                            best_port, best_value = other_port, port_value
+                if best_port:
+                    self.reassign_icebreaker(port, best_port, icebreaker)
+
+    def reassign_icebreaker(self, port: Port, best_port: Port, icebreaker: Icebreaker):
+        
+        travel_time = self.calculate_icebreaker_travel_time_to_port(port, best_port)
+        arrival_date = self.current_date + datetime.timedelta(days=travel_time)
+        
+        self.record_route(departure_date=self.current_date, 
+                          arrival_date=arrival_date, 
+                          departure_port=port.port_name,
+                          arrival_port_port=best_port.port_name, 
+                          movement_type='icebreaker',
+                          participants=icebreaker)
+    
+    def plan_remaining_ships(self, port: Port):
+        for ship in port.ships:
+            # Check if the ship is part of a second-best caravan
+            # ... add logic for second-best caravan check -- if not smth else (???)
+
+            # If not, assign independent departure date
+            departure_date = ship.ready_date
+            travel_time = calculate_ship_travel_time(ship, None)
+            arrival_date = departure_date + datetime.timedelta(days=travel_time)
+            
+            self.record_route(departure_date=departure_date, 
+                          arrival_date=arrival_date, 
+                          departure_port=port.port_name,
+                          arrival_port_port=ship.destination, 
+                          movement_type='ship',
+                          participants=ship)
+    
+    def record_caravan_route(self, caravan: Caravan, port: Port):
+        # Set departure and arrival dates
+        caravan.departure_date = max(ship.ready_date for ship in caravan.ships)
+        travel_time = calculate_caravan_travel_time(caravan)
+        arrival_date = caravan.departure_date + datetime.timedelta(days=travel_time)
+        caravan.icebreaker.available_date = arrival_date
+
+        participants = [ship for ship in caravan.ships] + [caravan.icebreaker]
+        self.record_route(departure_date=caravan.departure_date, 
+                          arrival_date=arrival_date, 
+                          departure_port=port.port_name,
+                          arrival_port_port=caravan.ships[0].destination, 
+                          movement_type='caravan',
+                          participants=participants)
+
+        #print(f"[LOG] Caravan with icebreaker {caravan.icebreaker.icebreaker_id} departs from {port.port_name} on {caravan.departure_date} to {caravan.ships[0].destination} arriving at {arrival_date}.")
+    
+    def record_route(self, departure_date: datetime.date, arrival_date: datetime.date, 
+                     departure_port: str, arrival_port: str, movement_type: str, 
+                     participants: List[str]):
+        
+        self.schedule.add_route(departure_date, arrival_date, departure_port, arrival_port, movement_type, participants)
+
+    def update_schedule(self):
+        routes_today = self.schedule.get_routes_by_date(self.current_date)
+        for route in routes_today:
+            if route['departure_date'] == self.current_date:
+                # Handle departure
+                self.handle_departure(route)
+            elif route['arrival_date'] == self.current_date:
+                # Handle arrival
+                self.handle_arrival(route)
+            else:
+                print(f'WARNING: Can not handle route. Date: {self.current_date}. Route: {route}')
+
+    def handle_departure(self, route: Dict):
+        port = self.ports[route['departure_port']]
+        destination_port = self.ports[route['arrival_port']]
+        if route['movement_type'] == 'caravan':
+            icebreaker = None
+            for participant_id in route['participants']:
+                ship = next((s for s in port.ships if s.ship_id == participant_id), None)
+                if ship:
+                    port.remove_ship(ship)
+                else:
+                    icebreaker = next((ib for ib in port.icebreakers if ib.icebreaker_id == participant_id), None)
+                    if icebreaker:
+                        port.remove_icebreaker(icebreaker)
+                        icebreaker.current_port = None
+                        destination_port.add_arriving_icebreaker(icebreaker, route['arrival_date'])
+        elif route['movement_type'] == 'icebreaker':
+            icebreaker = next((ib for ib in port.icebreakers if ib.icebreaker_id == route['participants'][0]), None)
+            if icebreaker:
+                port.remove_icebreaker(icebreaker)
+                icebreaker.current_port = None
+                destination_port.add_arriving_icebreaker(icebreaker, route['arrival_date'])
+        elif route['movement_type'] == 'ship':
+            ship = next((s for s in port.ships if s.ship_id == route['participants'][0]), None)
+            if ship:
+                port.remove_ship(ship)
+
+    def handle_arrival(self, route: Dict):
+        port = self.ports[route['arrival_port']]
+        if route['movement_type'] == 'caravan' or route['movement_type'] == 'icebreaker':
+            for participant_id in route['participants']:
+                icebreaker = next((ib for ib in port.arriving_icebreakers.values() if ib.icebreaker_id == participant_id), None)
+                if icebreaker:
+                    port.add_icebreaker(icebreaker)
+                    port.remove_arriving_icebreaker(icebreaker)
+        if route['movement_type'] == 'caravan' or route['movement_type'] == 'ship':
+            for participant_id in route['participants']:
+                ship = next((s for s in port.ships if s.ship_id == participant_id), None)
+                if ship:
+                    port.add_ship(ship)
 
     def calculate_icebreaker_travel_time_to_port(self, from_port: Port, to_port: Port) -> int:
         # Placeholder function to calculate travel time between ports
         return 5  # Example fixed travel time
-
-    def reassign_icebreaker(self, port: Port):
-        best_port = None
-        best_value = float('-inf')
-
-        for other_port in self.ports.values():
-            if other_port == port:
-                continue
-            best_caravan_quality = float('-inf')
-            for destination, ships in self.group_ships_by_destination(other_port.ships).items():
-                for size in range(1, self.max_in_caravan + 1):
-                    for comb in itertools.combinations(ships, size):
-                        caravan = Caravan(list(comb), None, self.max_in_caravan)
-                        if caravan.caravan_quality > best_caravan_quality:
-                            best_caravan_quality = caravan.caravan_quality
-            travel_time = self.calculate_icebreaker_travel_time_to_port(port, other_port)
-            port_value = best_caravan_quality - travel_time
-            if port_value > best_value:
-                best_value = port_value
-                best_port = other_port
-
-        if best_port:
-            # Assign an icebreaker to the best port
-            best_caravan = max(best_port.icebreakers, key=lambda x: x.available_date)
-            port.add_icebreaker(best_caravan)
-            best_port.remove_icebreaker(best_caravan)
-            travel_time = self.calculate_icebreaker_travel_time_to_port(port, best_port)
-            arrival_date = self.current_date + datetime.timedelta(days=travel_time)
-            port.add_arriving_icebreaker(best_caravan, arrival_date)
-
